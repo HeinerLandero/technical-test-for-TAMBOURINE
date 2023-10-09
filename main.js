9b78a3c889c656800c197fec3c7670fc429e221c
Heiner Landero <heinerlandero@gmail.com>
2023-10-08 19:01:49 -0500

    Update 08/10/2023

--- main.js
+++ main.js
@@ -1,3 +1,38 @@
+document.addEventListener('DOMContentLoaded', function () {
+  const counterElement = document.getElementById('counter');
+  const incrementButton = document.getElementById('right-arrow');
+  const decrementButton = document.getElementById('left-arrow');
+
+  let count = 0;
+
+  function updateCounter() {
+    counterElement.textContent = count.toString().padStart(2, '0');
+  }
+
+  incrementButton.addEventListener('click', function () {
+      count += 1;
+      updateCounter();
+  });
+
+  decrementButton.addEventListener('click', function () {
+      count -= 1;
+      updateCounter();
+  });
+
+  document.addEventListener('keydown', function (event) {
+      if (event.key === 'ArrowRight') {
+          count += 1; 
+          updateCounter();
+      } else if (event.key === 'ArrowLeft') {
+          count -= 1; 
+          updateCounter();
+      }
+  });
+});
+
+
+
+/* Swiper */
 var swiper = new Swiper(".mySwiper", {
     navigation: {
       nextEl: ".swiper-button-next",
